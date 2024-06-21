fe21ac9a2b03bdc748b3c4cda2684cc27027805b
IgorSantanaM <igorsantanamedeiros17@gmail.com>
2024-06-21 16:36:18 -0300

    Initial commit

---
+++ EmployeePayRoll/CommissionEmployee.cs
@@ -0,0 +1,58 @@
+﻿using System;
+public class CommissionEmployee : Employee
+{
+    private decimal grossSales; // gross weekly sales
+    private decimal commissionRate; // commission percentage
+
+    // five-parameter constructor
+    public CommissionEmployee(string first, string last, string ssn,
+        decimal sales, decimal rate)
+        : base(first, last, ssn) // Call the base class constructor
+    {
+        GrossSales = sales; // validate gross sales via property
+        CommissionRate = rate; // validate commission rate via property
+    } // end five-parameter CommissionEmployee constructor
+    public decimal GrossSales
+    {
+        get
+        {
+            return grossSales;
+        } // end get
+        set
+        {
+            if (value >= 0)
+                grossSales = value;
+            else
+                throw new ArgumentOutOfRangeException(
+                    "GrossSales", value, "GrossSales must be >= 0");
+        } // end set
+    } // end property GrossSales
+
+    public decimal CommissionRate
+    {
+        get
+        {
+            return commissionRate;
+        } // end get
+        set
+        {
+            if (value > 0 && value < 1)
+                commissionRate = value;
+            else
+                throw new ArgumentOutOfRangeException(
+                    "CommissionRate", value, "CommissionRate must be > 0 and < 1");
+        } // end set
+    } // end property CommissionRate
+
+    public override decimal Earnings()
+    {
+        return CommissionRate * GrossSales;
+    } // end method Earnings
+
+    public override string ToString()
+    {
+        return string.Format("{0}: {1}\n{2}: {3:C}\n{4}: {5:F2}",
+            "commission employee", base.ToString(),
+            "gross sales", GrossSales, "commission rate", CommissionRate);
+    } // end method ToString
+} // end class CommissionEmployee
---
+++ EmployeePayRoll/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public abstract class Employee
+{
+    public string FirstName { get; private set; }
+
+    public string LastName { get; private set; }
+
+    public string SocialSecurityNumber { get; private set; }
+    public Employee(string first, string last, string ssn)
+    {
+        FirstName = first;
+        LastName = last;
+        SocialSecurityNumber = ssn;
+    } // end three-parameter Employee constructor
+
+    public override string ToString()
+    {
+        return string.Format("{0} {1}\nsocial security number: {2}",
+            FirstName, LastName, SocialSecurityNumber);
+    } // end method ToString
+
+    public abstract decimal Earnings(); // no implementation here
+} // end abstract class Employee
